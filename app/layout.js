a88201a223abc2beff2e513abcf429aaadeba1ad
folkmoz1 <63143147+folkmoz1@users.noreply.github.com>
2023-04-07 20:22:44 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -22,7 +22,6 @@ export default function RootLayout({ children }) {
       <body className="min-h-screen">
         <PageFadeIn>{children}</PageFadeIn>
         <TailwindIndicator />
-        <Background />
       </body>
     </html>
   );
