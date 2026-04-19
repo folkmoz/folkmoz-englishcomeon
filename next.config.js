8238c6c96463087335bf67b735b89bf9f9ed3bba
FOLKMOZ <folkmoz1@gmail.com>
2026-04-19 22:32:59 +0700

    Remove standalone output option from Next.js config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
   reactStrictMode: true,
-  reactCompiler: true
+  reactCompiler: true,
 };
 
 module.exports = nextConfig;
